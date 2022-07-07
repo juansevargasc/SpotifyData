29fda02d2fb8d9a1c6200aeeb7e564d3a7761a4c
Juanse Vargas <jsvargasc@unal.edu.co>
2022-07-07 14:12:14 -0500

    Relationships on DB setup, DB migrated

--- application/routes.py
+++ application/routes.py
@@ -15,7 +15,8 @@ def ping_route():
 def health():
     return jsonify({'msg': 'Hello World'})
 
-# Artist
+
+# ARTIST
 # Create
 @app.route('/artists', methods=['POST'])
 def add_artist():
@@ -30,7 +31,6 @@ def add_artist():
     db.session.commit()
 
     return artist_schema.jsonify(new_artist)
-
 # Read all
 @app.route('/artists', methods=['GET'])
 def get_artists():
@@ -38,13 +38,11 @@ def get_artists():
     result = artists_schema.dump(all_artists)
 
     return jsonify(result)
-
 # Read one
 @app.route('/artists/<id>', methods=['GET'])
 def get_artist(id):
     artist = Artist.query.get(id)
     return artist_schema.jsonify(artist)
-
 # Update one
 @app.route('/artists/<id>', methods=['PUT'])
 def update_artist(id):
@@ -64,7 +62,6 @@ def update_artist(id):
     db.session.commit()
 
     return artist_schema.jsonify(artist)
-
 # Delete one
 @app.route('/artists/<id>', methods=['DELETE'])
 def delete_artist(id):
@@ -77,3 +74,63 @@ def delete_artist(id):
     db.session.commit()
 
     return artist_schema.jsonify(artist)
+
+# ALBUM ###################
+# # Create
+# @app.route('/albums', methods=['POST'])
+# def add_artist():
+#     id = request.json['id']
+#     name = request.json['name']
+#     url = request.json['url']
+#     followers = request.json['followers']
+
+#     new_artist = Artist(id, name, url, followers)
+
+#     db.session.add(new_artist)
+#     db.session.commit()
+
+#     return artist_schema.jsonify(new_artist)
+# # Read all
+# @app.route('/artists', methods=['GET'])
+# def get_artists():
+#     all_artists = Artist.query.all()
+#     result = artists_schema.dump(all_artists)
+
+#     return jsonify(result)
+# # Read one
+# @app.route('/artists/<id>', methods=['GET'])
+# def get_artist(id):
+#     artist = Artist.query.get(id)
+#     return artist_schema.jsonify(artist)
+# # Update one
+# @app.route('/artists/<id>', methods=['PUT'])
+# def update_artist(id):
+#     # Get artist
+#     artist = Artist.query.get(id)
+
+#     name = request.json['name']
+#     url = request.json['url']
+#     followers = request.json['followers']
+
+#     # Update python class
+#     artist.name = name
+#     artist.url = url
+#     artist.followers = followers
+
+#     # Update on DB
+#     db.session.commit()
+
+#     return artist_schema.jsonify(artist)
+# # Delete one
+# @app.route('/artists/<id>', methods=['DELETE'])
+# def delete_artist(id):
+#     # Get artist
+#     artist = Artist.query.get(id)
+#     # Delete that artist
+#     db.session.delete(artist)
+
+#     # Commit that change
+#     db.session.commit()
+
+#     return artist_schema.jsonify(artist)
+
